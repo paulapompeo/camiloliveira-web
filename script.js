d5d0c03006e1ab1c82ccebc44032792b7c078e28
Paula Pompeo <paulapompeoc@gmail.com>
2024-01-11 22:25:15 +0100

    fix modals

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ aboutBtn.addEventListener('click', function() {
 });
 
 aboutModal.addEventListener('click', function(event) {
-  if (event.target.classList.contains('fechar')) {
+  if (event.target.classList.contains('close')) {
     aboutModal.style.display = 'none';
     document.body.style.overflow = 'auto'; 
   }
@@ -26,7 +26,7 @@ shippingBtn.addEventListener('click', function() {
 })
 
 shippingModal.addEventListener('click', function(event) {
-  if (event.target.classList.contains('fechar')) {
+  if (event.target.classList.contains('close')) {
     shippingModal.style.display = 'none';
     document.body.style.overflow = 'auto'; 
   }
@@ -38,9 +38,18 @@ sizeGuideBtn.addEventListener('click', function() {
 })
 
 sizeGuideModal.addEventListener('click', function(event) {
-  if (event.target.classList.contains('fechar')) {
+  if (event.target.classList.contains('close')) {
     sizeGuideModal.style.display = 'none';
     document.body.style.overflow = 'auto'; 
+  }
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    aboutModal.style.display = 'none';
+    shippingModal.style.display = 'none';
+    sizeGuideModal.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 });
 
